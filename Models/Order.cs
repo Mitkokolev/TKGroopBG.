2612f2c4c2581f77cc8aea895285c487633d85d9
MitkoKolev <User@DESKTOP-V9E8UOQ>
2026-02-16 01:45:42 +0200

    error fix

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace TKGroopBG.Models
@@ -7,16 +6,16 @@ namespace TKGroopBG.Models
     public class Order
     {
         public int Id { get; set; }
-
-
         public string? Address { get; set; }
         public string? Comment { get; set; }
-
         public string CartJson { get; set; } = "";
-
         public decimal TotalPrice { get; set; }
 
-
+        public string CustomerName { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Status { get; set; } = "Нова";
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
 
